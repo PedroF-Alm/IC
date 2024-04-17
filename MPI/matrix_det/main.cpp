37b8ffe520ec7bc8bf09afb61fbfa549e945cb74
Pedro Fernandes <pedrofernandesacc@gmail.com>
2024-04-17 08:19:58 -0300

    Change MPI/matrix_det/main.cpp

--- MPI/matrix_det/main.cpp
+++ MPI/matrix_det/main.cpp
@@ -3,36 +3,18 @@
 #include <time.h>
 #include <mpi.h>
 
-#define N 200
+#define N 3
 #define MAX 20
 #define MIN 0
 
-//#define PRINT
+// #define PRINT
 
-int **matrix = NULL;
+int **matrix = NULL, **submatrix = NULL;
 
-int determinant(int**, int, int, int);
+int determinant(int **, int);
 
 int main(int argc, char **argv)
 {
-    srand(0);
-
-    int min = MIN, max = MAX;
-
-    if (argc > 2)
-    {
-        min = atoi(argv[1]);
-        max = atoi(argv[2]);
-    }
-
-    matrix = (int**) calloc(N, sizeof(int*));
-    for (int i = 0; i < N; i++)
-    {
-        matrix[i] = (int*) calloc(N, sizeof(int));
-        for (int j = 0; j < N; j++)
-            matrix[i][j] = (int)((double)rand() / RAND_MAX * (max - min + 1)) + min;
-    }
-
     // MPI
     MPI_Init(NULL, NULL);
 
@@ -48,30 +30,116 @@ int main(int argc, char **argv)
 
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
     // ____________________________________________
 
-    int det = determinant(matrix, N, world_rank, world_size);
-    printf("%d\n", det);
+    if (world_rank == 0)
+    {
+        int det = 0;
+
+        matrix = (int **)malloc(sizeof(int *) * N);
+        submatrix = (int **)malloc(sizeof(int *) * (N - 1));
+
+        srand(0);
+
+        int min = MIN, max = MAX;
+
+        if (argc > 2)
+        {
+            min = atoi(argv[1]);
+            max = atoi(argv[2]);
+        }
+        for (int i = 0; i < N; i++)
+        {
+            matrix[i] = (int *)malloc(sizeof(int) * N);
+            if (i < N - 1)
+                submatrix[i] = (int *)malloc(sizeof(int) * (N - 1));
+            for (int j = 0; j < N; j++)
+                matrix[i][j] = (int)((double)rand() / RAND_MAX * (max - min + 1)) + min;
+        }
+
+        // expand along first row
+        for (int col = 0; col < N; col++)
+        {
+            // copy into minor matrix, left side then right side
+            for (int r = 1; r < N; r++)
+            {
+                for (int c = 0; c < col; c++)
+                {
+                    submatrix[r - 1][c] = matrix[r][c];
+                }
+                for (int c = col + 1; c < N; c++)
+                {
+                    submatrix[r - 1][c - 1] = matrix[r][c];
+                }
+            }
+
+            // use "minor" matrix at this point to calculte
+            // its determinant
+            MPI_Send(&(matrix[0][col]), 1, MPI_INT, 1 + (col % world_size - 1), 0, MPI_COMM_WORLD);
+            MPI_Send(submatrix, (N - 1) * (N - 1), MPI_INT, 1 + (col % world_size - 1), 1, MPI_COMM_WORLD);
+        }
+
+        for (int worker = 1; worker < world_size; worker++)
+        {
+            int d;
+            MPI_Recv(&d, 1, MPI_INT, worker, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            det += d;
+        }
+
+        printf("Determinant: %d\n", det);
+
+        for (int i = 0; i < N; i++)
+            free(matrix[i]);
+        free(matrix);
+
+        for (int i = 0; i < N - 1; i++)
+            free(submatrix[i]);
+        free(submatrix);
+    }
+    else
+    {
+        submatrix = (int **)malloc(sizeof(int *) * (N - 1));
+        for (int i = 0; i < N - 1; i++)
+            submatrix[i] = (int *)malloc(sizeof(int) * (N - 1));
+
+        int det = 0;
+        for (int col = 0; col < N; col += world_size - 1)
+        {
+            int cofactor;
+            int s = col % 2 == 0 ? -1 : -1;
+            MPI_Recv(&cofactor, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Recv(submatrix, (N - 1) * (N - 1), MPI_INT, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            det += s * cofactor * determinant(matrix, N - 1);
+        }
+
+        MPI_Send(&det, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
+
+        for (int i = 0; i < N - 1; i++)
+            free(submatrix[i]);
+        free(submatrix);
+    }
+
     // ____________________________________________
 
     MPI_Finalize();
-
-    for (int i = 0; i < N; i++)
-        free(matrix[i]);
-    free(matrix);
 
     return 0;
 }
 
 // Function to calculate the determinant
 // of a matrix
-int determinant(int **matrix, int size, int world_rank, int world_size)
+int determinant(int **matrix, int size)
 {
-    int det = 0, aux = 0;
+    int det = 0;
     int sign = 1;
 
-    if (world_rank != 0)
-        MPI_Recv(&matrix, size*size, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    if (matrix == NULL)
+        return 0;
+
+    for (int i = 0; i < size; i++)
+        if (matrix[i] == NULL)
+            return 0;
 
     // Base Case
     if (size == 1)
@@ -111,33 +179,14 @@ int determinant(int **matrix, int size, int world_rank, int world_size)
                 sub_j = 0;
             }
 
-
-            if (world_rank == 0) {
-                MPI_Send(&cofactor, (size - 1) * (size - 1), MPI_INT, i % world_size + 1, 0, MPI_COMM_WORLD);
-            }
-            else {
-                det += sign * matrix[0][i] * determinant(cofactor, size - 1, world_rank, world_size); 
-            }
+            // Update the determinant value
+            det += sign * matrix[0][i] * determinant(cofactor, size - 1);
             sign = -sign;
-
             for (int j = 0; j < size - 1; j++)
             {
                 delete[] cofactor[j];
             }
             delete[] cofactor;
-        }
-
-        if (world_rank == 0)
-        {
-            for (int i = 0; i < size; i++)
-            {
-                MPI_Recv(&aux, i % world_size - 1, MPI_INT, i, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                det += aux;
-            }
-        }
-        else
-        {
-            MPI_Send(&det, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
         }
     }
 
